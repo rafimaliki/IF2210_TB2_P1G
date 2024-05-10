d404041804011dcb399aaa6bc9b00095a9fd2f2d
Ahmad Rafi Maliki <114818634+rafimaliki@users.noreply.github.com>
2024-05-10 13:59:27 +0700

    Delete src/HarvestMoonCombat directory

--- src/HarvestMoonCombat/src/main/java/com/game/harvestmooncombat/pkg/KartuProduk.java
+++
@@ -1,6 +0,0 @@
-package com.game.harvestmooncombat.pkg;
-
-public class KartuProduk extends Kartu {
-    private int hargaJual;
-    private int beratTambah;
-}
--- src/HarvestMoonCombat/src/main/java/com/game/harvestmooncombat/pkg/KartuTanaman.java
+++
@@ -1,9 +0,0 @@
-package com.game.harvestmooncombat.pkg;
-
-public class KartuTanaman extends Kartu {
-    final int UMUR_PANEN_JAGUNG = 3;
-    final int UMUR_PANEN_LABU = 5;
-    final int UMUR_PANEN_STROBERI = 4;
-
-    private int umur;
-}

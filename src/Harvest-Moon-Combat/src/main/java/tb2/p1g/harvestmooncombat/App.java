3c4f6971dc7b6578446aa5930fb9c1b95684902a
Ahmad Rafi Maliki <rafimaliki28@gmail.com>
2024-05-24 22:54:14 +0700

    feat: audio nyolong

--- src/Harvest-Moon-Combat/src/main/java/tb2/p1g/harvestmooncombat/App.java
+++ src/Harvest-Moon-Combat/src/main/java/tb2/p1g/harvestmooncombat/App.java
@@ -2,6 +2,7 @@ package tb2.p1g.harvestmooncombat;
 import javafx.application.Application;
 
 import javafx.stage.Stage;
+import tb2.p1g.harvestmooncombat.Audio.AudioFactory;
 import tb2.p1g.harvestmooncombat.Models.GameManager;
 import tb2.p1g.harvestmooncombat.Views.ViewFactory;
 
@@ -13,9 +14,15 @@ public class App extends Application {
         GameManager gameManager = GameManager.getInstance();
         gameManager.startGame();
 
-        ViewFactory vf = new ViewFactory();
-        vf.setPrimaryStage(stage);
+        ViewFactory.setPrimaryStage(stage);
 
+        String farmAudio = getClass().getResource("/Audio/Farm.mp3").toExternalForm();
+        String bearAudio = getClass().getResource("/Audio/BearAttack.mp3").toExternalForm();
+
+        AudioFactory.addAudio("farm", farmAudio);
+        AudioFactory.addAudio("bear", bearAudio);
+
+        AudioFactory.playAudio("farm");
         ViewFactory.LoadMainScreen();
     }
 
